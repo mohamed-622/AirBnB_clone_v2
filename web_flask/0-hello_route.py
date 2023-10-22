d2caf88b3d861f7d5e5f5e2191c5fda90d39cbfa
mohamed-622 <mohamedezgoudi@gmail.com>
2023-10-22 18:46:36 +0100

    pep8

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+"""Start a Flask web application"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
-# Define a route for the root URL
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Return a simple string for index page"""
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
